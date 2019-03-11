d468467919ca20b9a1f2726cb692278a9192a204
anniebaker <bakeranneabb@gmail.com>
2019-03-10 19:07:21 -0500

    to-do

---
+++ myPortfolio/src/cookie-game/cookie-game.js
@@ -0,0 +1,21 @@
+let classes = 0; //Declares that number of classes begins at zero
+
+//Creates a function that increases the number of classes displayed, connected to the onclick function
+function classClicker(number){
+    classes = classes + number;
+    document.getElementById("classes").innerHTML = classes;
+};
+
+//Declares that the number of degrees begins at zero
+let degrees = 0;
+
+//Creates a function that establishes how much a degree costs, and what happens when a degree is bought
+function getDegree(){
+    let degreeCost = 2   
+    if(classes >= degreeCost){         
+        degrees = degrees + 1;                          
+    	classes = classes - degreeCost;                         
+        document.getElementById('degrees').innerHTML = degrees;  
+        document.getElementById('classes').innerHTML = classes;
+    };
+};
