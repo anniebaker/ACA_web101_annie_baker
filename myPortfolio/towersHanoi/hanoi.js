63ce61a01d6c87adf05ecb237360edb1a63892ea
anniebaker <bakeranneabb@gmail.com>
2019-03-18 19:12:42 -0500

    javascript practice

---
+++ myPortfolio/towersHanoi/hanoi.js
@@ -0,0 +1,9 @@
+// let pieceSmall = $('.piece-small') 
+// $(".piece").draggable();
+// $(".piece-med").draggable();
+// $(".piece-large").draggable();
+$(document).ready(function() {
+    $(function() {
+        $( "#small" ).draggable();
+  } );
+}
